e25ad61da332c767b32c001f55bd12ee7453cbcc
adhewahyu <51067003+adhewahyu@users.noreply.github.com>
2023-04-10 08:14:55 +0700

    update scheduler housekeep log

--- audit-service/src/main/java/com/dan/auditservice/repository/LogRepository.java
+++ audit-service/src/main/java/com/dan/auditservice/repository/LogRepository.java
@@ -3,16 +3,9 @@ package com.dan.auditservice.repository;
 import com.dan.auditservice.model.entity.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LogRepository extends JpaRepository<Log, String>, JpaSpecificationExecutor<Log> {
-
-    @Modifying(clearAutomatically = true)
-    @Query(value = "DELETE FROM AUDITS WHERE CREATED_DATE <  (CURRENT_DATE - interval :interval)", nativeQuery = true)
-    void doHouskeepAudit(@Param("interval") String interval);
 
 }
--- audit-service/src/main/java/com/dan/auditservice/service/HousekeepLogService.java
+++ audit-service/src/main/java/com/dan/auditservice/service/HousekeepLogService.java
@@ -1,25 +1,28 @@
 package com.dan.auditservice.service;
 
-import com.dan.auditservice.repository.LogRepository;
 import com.dan.shared.sharedlibrary.model.request.BaseRequest;
 import com.dan.shared.sharedlibrary.model.response.ValidationResponse;
 import com.dan.shared.sharedlibrary.service.BaseService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class HousekeepLogService implements BaseService<BaseRequest, ValidationResponse> {
 
-    private final LogRepository logRepository;
-
     @Value("${config.housekeep.interval.audit}")
     private String intervalHousekeepAudit;
 
+    private final JdbcTemplate jdbcTemplate;
+
     @Override
     public ValidationResponse execute(BaseRequest input) {
-        logRepository.doHouskeepAudit(intervalHousekeepAudit);
+        String query = " DELETE FROM logs WHERE created_date < (CURRENT_DATE - interval '"+ intervalHousekeepAudit + "') ";
+        jdbcTemplate.execute(query);
         return ValidationResponse.builder().result(true).build();
     }
     

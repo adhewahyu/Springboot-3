4d301e1d02062bedd691beb737e86ea86ee549c4
adhewahyu <51067003+adhewahyu@users.noreply.github.com>
2023-08-11 19:04:00 +0700

    (user-service): add logging aspect

---
+++ user-service/src/main/java/com/dan/userservice/aspect/LoggingAspect.java
@@ -0,0 +1,34 @@
+package com.dan.userservice.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Pointcut( "within(com.dan.userservice.controller..*)" +
+            " || within(com.dan.userservice.service..*)" +
+            " || within(com.dan.userservice.adaptor..*)")
+    public void applicationPackagePointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    @Around("applicationPackagePointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (log.isInfoEnabled()) {
+            log.info("{}.{}() - called", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+        }
+        Object result = joinPoint.proceed();
+        if (log.isInfoEnabled()) {
+            log.info("{}.{}() - end", joinPoint.getSignature().getDeclaringTypeName(), joinPoint.getSignature().getName());
+        }
+        return result;
+    }
+
+}

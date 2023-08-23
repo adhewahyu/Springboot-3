8448e1e0654cfa08818bdc21ef52befb926d4375
adhewahyu <51067003+adhewahyu@users.noreply.github.com>
2023-08-23 09:40:30 +0700

    (user-service): adjust code role response [fix]

--- user-service/src/main/java/com/dan/userservice/model/response/RoleResponse.java
+++ user-service/src/main/java/com/dan/userservice/model/response/RoleResponse.java
@@ -1,6 +1,7 @@
 package com.dan.userservice.model.response;
 
 import com.dan.shared.sharedlibrary.model.response.BaseResponse;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RoleResponse extends BaseResponse {
 
     private String id;

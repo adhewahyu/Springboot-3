21848be502eb50977c47ef8035eb755055f99f32
adhewahyu <51067003+adhewahyu@users.noreply.github.com>
2023-10-09 07:23:17 +0700

    Update permission_script.sql

--- user-service/src/main/resources/scripts/permission_script.sql
+++ user-service/src/main/resources/scripts/permission_script.sql
@@ -10,11 +10,11 @@ VALUES('70f365de-7109-4f45-a08a-d2d36c990df2', 1, '', 'user', '[PARENT]-Update U
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
 VALUES('375a3fa7-d80b-46e6-b1f1-7ea9d31d4823', 2, '/user/v1/update', 'user', '[CHILD]-API Update User', 'API Update User', '70f365de-7109-4f45-a08a-d2d36c990df2', 1);
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
-VALUES('0f324f23-61c6-49d3-a3f6-edb224284437', 1, '', 'user', 'Delete User', 'Delete User', '6cf53724-cecb-4145-89d8-03ee33310694', 1);
+VALUES('0f324f23-61c6-49d3-a3f6-edb224284437', 1, '', 'user', '[PARENT]-Delete User', 'Delete User', '6cf53724-cecb-4145-89d8-03ee33310694', 1);
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
 VALUES('d5a91e52-9480-4a44-b523-88b8ae9c780b', 2, '/user/v1/delete', 'user', '[CHILD]-API Delete User', 'API Delete User', '0f324f23-61c6-49d3-a3f6-edb224284437', 1);
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
-VALUES('d66be642-a30d-4f33-bd95-73cae189c57b', 1, '', 'user', 'View User', 'View User', '6cf53724-cecb-4145-89d8-03ee33310694', 1);
+VALUES('d66be642-a30d-4f33-bd95-73cae189c57b', 1, '', 'user', '[PARENT]-View User', 'View User', '6cf53724-cecb-4145-89d8-03ee33310694', 1);
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
 VALUES('d4b5ae8d-a56e-4b68-9f72-de89bf40220b', 2, '/user/v1/info', 'user', '[CHILD]-API View User Info', 'API View User Info', 'd66be642-a30d-4f33-bd95-73cae189c57b', 1);
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
@@ -36,7 +36,7 @@ VALUES('fec2d802-9b2c-4f14-b078-3605a63659d6', 1, '', 'role', '[PARENT]-Delete R
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
 VALUES('6591f7f6-4dfb-4a59-a9b6-b08e65be0109', 2, '/role/v1/delete', 'role', '[CHILD]-API Delete Role', 'API Delete Role', 'fec2d802-9b2c-4f14-b078-3605a63659d6', 1);
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
-VALUES('894f8bcd-b361-45c4-a37f-29fd61bdbd35', 1, '', 'role', 'View Role', 'View Role', '57a840e6-3ba5-4c7d-a3ef-ec8931772a03', 1);
+VALUES('894f8bcd-b361-45c4-a37f-29fd61bdbd35', 1, '', 'role', '[PARENT]-View Role', 'View Role', '57a840e6-3ba5-4c7d-a3ef-ec8931772a03', 1);
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
 VALUES('e945de4d-32fc-435b-8e8d-470ad799984a', 2, '/role/v1/info', 'role', '[CHILD]-API View Role Info', 'API View Role Info', '894f8bcd-b361-45c4-a37f-29fd61bdbd35', 1);
 INSERT INTO public.permissions (id, access_level, api, category, description, "name", parent_id, status)
